f89d0c5dc7099510df6f643a9651475f9b1b5f30
eduardomarques00 <108638567+eduardomarques00@users.noreply.github.com>
2023-02-01 20:02:43 -0300

    condições e teclas configuradas

--- main.js
+++ main.js
@@ -16,5 +16,13 @@ for(let contador = 0; contador < ListaDeAnimais.length;contador++){
         tocaSom(audio)
     }
 
-    
+    SonsAnimais.onkeydown = function(evento){
+console.log(evento.code ==='Space')
+        if(evento.code == 'Space' || evento.code === 'Enter' ){
+        SonsAnimais.classList.add('ativa')
+        }
+    }
+    SonsAnimais.onkeyup = function(){
+        SonsAnimais.classList.remove('ativa')
+    }
 }
